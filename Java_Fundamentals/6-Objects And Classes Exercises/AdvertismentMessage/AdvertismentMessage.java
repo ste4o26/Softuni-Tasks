782cd7ebc752a0859551038ffba1627be4742e19
ste4o26 <36564989+ste4o26@users.noreply.github.com>
2020-02-23 08:36:06 +0000

    Create AdvertismentMessage.java

---
+++ Java_Fundamentals/6-Objects And Classes Exercises/AdvertismentMessage/AdvertismentMessage.java
@@ -0,0 +1,20 @@
+package AdvertismentMessage;
+
+public class Message {
+    private String phrase;
+    private  String event;
+    private String author;
+    private String city;
+
+    public Message(String phrase, String event, String author, String city) {
+        this.phrase = phrase;
+        this.event = event;
+        this.author = author;
+        this.city = city;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %s %s - %s.", this.phrase, this.event, this.author, this.city);
+    }
+}
